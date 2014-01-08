60f4958e22263e3800b0cb5b594e1c7628abc3e3
hjacobs <henning@jacobs1.de>
2014-01-08 21:16:06 +0100

    add MTP support

--- import.py
+++ import.py
@@ -4,9 +4,14 @@
 import argparse
 import datetime
 import logging
+import re
+import subprocess
+import tempfile
 import qif
 
 from gnucash import Session, Transaction, Split, GncNumeric
+
+MTP_SCHEME = 'mtp:'
 
 
 def lookup_account_by_path(root, path):
@@ -53,6 +58,52 @@ def add_transaction(book, item, currency):
     tx.CommitEdit()
 
 
+def read_entries_from_mtp_file(file_id, filename):
+    with tempfile.NamedTemporaryFile(suffix=filename) as fd:
+        subprocess.check_call(['mtp-getfile', file_id, fd.name])
+        entries_from_qif = qif.parse_qif(fd)
+    logging.debug('Read %s entries from %s', len(entries_from_qif), filename)
+    return entries_from_qif
+
+
+def get_mtp_files():
+    '''list all files on MTP device and return a tuple (file_id, filename) for each file'''
+
+    # using mtp-tools instead of pymtp because I could not get pymtp to work (always got segmentation fault!)
+    out = subprocess.check_output('mtp-files 2>&1', shell=True)
+    last_file_id = None
+    for line in out.splitlines():
+        cols = line.strip().split(':', 1)
+        if len(cols) == 2:
+            key, val = cols
+            if key.lower() == 'file id':
+                last_file_id = val.strip()
+            elif key.lower() == 'filename':
+                filename = val.strip()
+                yield (last_file_id, filename)
+
+
+def read_entries_from_mtp(pattern):
+    entries = []
+    regex = re.compile(pattern)
+    for file_id, filename in get_mtp_files():
+        if regex.match(filename):
+            logging.debug('Found matching file on MTP device: "%s" (ID: %s)', filename, file_id)
+            entries.extend(read_entries_from_mtp_file(file_id, filename))
+    return entries
+
+
+def read_entries(fn):
+    logging.debug('Reading %s..', fn)
+    if fn.startswith(MTP_SCHEME):
+        items = read_entries_from_mtp(fn[len(MTP_SCHEME):])
+    else:
+        with open(fn) as fd:
+            items = qif.parse_qif(fd)
+    logging.debug('Read %s items from %s', len(items), fn)
+    return items
+
+
 def main(args):
     if args.verbose:
         lvl = logging.DEBUG
@@ -65,11 +116,7 @@ def main(args):
 
     all_items = []
     for fn in args.file:
-        logging.debug('Reading %s..', fn)
-        with open(fn) as fd:
-            items = qif.parse_qif(fd)
-            logging.debug('Read %s items from %s..', len(items), fn)
-            all_items.extend(items)
+        all_items.extend(read_entries(fn))
 
     logging.debug('Opening GnuCash file %s..', args.gnucash_file)
     session = Session(args.gnucash_file)
@@ -91,7 +138,8 @@ if __name__ == '__main__':
     parser.add_argument('-q', '--quiet', help='Silent mode, only log warnings', action='store_true')
     parser.add_argument('-c', '--currency', metavar='ISOCODE', help='Currency ISO code (default: EUR)', default='EUR')
     parser.add_argument('-f', '--gnucash-file', help='Gnucash data file')
-    parser.add_argument('file', nargs='+', help='Input QIF file(s)')
+    parser.add_argument('file', nargs='+',
+                        help='Input QIF file(s), can also be "mtp:<PATTERN>" to import from MTP device')
 
     args = parser.parse_args()
     main(args)
